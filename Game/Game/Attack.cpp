30b7b483ff408f725bd37f869fbbb47d951cb278
julianjauregui <jjauregui002@student.franciscan.edu>
2024-02-15 14:07:52 -0500

    Made the Attack Class

--- Game/Game/Attack.cpp
+++ Game/Game/Attack.cpp
@@ -1,2 +1,43 @@
 #include "Attack.h"
 //I will put all the functions for this class in here
+
+//returns damage multiplier
+double Attack::getDamageMultiplier()
+{
+	return damageMultiplier;
+}
+
+//returns defense multiplier
+double Attack::getDefenseMultiplier()
+{
+	return defenseMultiplier;
+}
+\
+//returns name of attack
+string Attack::getName()
+{
+	return name;
+}
+
+//Sets damageMultiplier equal to damage input
+void Attack::setDamageMultiplier(double damage)
+{
+	damageMultiplier = damage;
+}
+
+//sets defenseMultiplier equal to defense input
+void Attack::setDefenseMultiplier(double defense)
+{
+	defenseMultiplier = defense;
+}
+
+//sets name equal to nomen input
+void Attack::setName(string nomen)
+{
+	name = nomen;
+}
+
+
+
+
+
--- Game/Game/Attack.h
+++ Game/Game/Attack.h
@@ -1,6 +1,35 @@
+//this is the attack class, it is used for the battle class and is the basic moves that each character does in an attack
 #pragma once
+#include <string>
+using namespace std;
 class Attack
 {
-	//this is the attack class, I will probably implement this later
+protected:
+	//Has double for damage multiplier, which multiplies the attack power of a character in the battle
+	double damageMultiplier;
+	//Has double for defense multiplier, which multiplies the defense power of a character in the battle
+	double defenseMultiplier;
+	//Has string for the name of the attack
+	string name;
+public:
+	Attack(double damage, double defense, string nomen) {
+		damageMultiplier = damage;
+		defenseMultiplier = defense;
+		name = nomen;
+	}
+
+	//takes nothing and outputs the damage multiplier stat
+	double getDamageMultiplier();
+	//Takes nothing and outputs the defense multiplier stat
+	double getDefenseMultiplier();
+	//Takes nothing and outputs the name of the attack
+	string getName();
+
+	//sets damageMultiplier equal to damage
+	void setDamageMultiplier(double damage);
+	//sets defenseMultiplier equal to defense
+	void setDefenseMultiplier(double defense);
+	//sets the name equal to nomen
+	void setName(string nomen);
 };
 
