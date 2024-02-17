16ce9247016bfbb3101061f9a1410ef59998518f
julianjauregui <jjauregui002@student.franciscan.edu>
2024-02-17 13:01:42 -0500

    Added a toString function for attack class

--- Game/Game/Attack.cpp
+++ Game/Game/Attack.cpp
@@ -37,7 +37,11 @@ void Attack::setName(string nomen)
 	name = nomen;
 }
 
-
-
-
-
+//Takes the input of the damage stat from the character attacking and def stat and outputs the attack in a string
+string Attack::toString(double damage, double def)
+{
+	//makes a string called output that has all the information for the attack being done by a specific character
+	//Format will be: "$ATTACK does $NUMBER damage and increases the characters defense by $DEFENSE.
+	string output = name + " does " + (to_string(damage * damageMultiplier)) + " damage and increases the characters defense by " + (to_string(def * defenseMultiplier));
+	return output;
+}
