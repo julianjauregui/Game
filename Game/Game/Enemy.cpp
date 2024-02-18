27ff0424f1181de5bc66b783a3e1ea37d6bfd49b
czyk02rlu <143612038+czyk02rlu@users.noreply.github.com>
2024-02-18 11:44:49 -0500

    added comment about the function that generates random enemies

--- Game/Game/Enemy.cpp
+++ Game/Game/Enemy.cpp
@@ -1,1 +1,11 @@
+
+//Imlementation of the Character class, from the Character.h file
 #include "Enemy.h"
+
+//generates random enemy.  takes nothing in, creates an enemy with randiomly selected name, randomizes other character stats
+static Enemy generateEnemy() 
+{
+    std::string names[] = { "Goblin", "Skeleton", "Orc", "Troll" };
+    int index = rand() % 4;
+    return Enemy(names[index], rand() % 20 + 10, rand() % 5 + 5); //not right, needs to reflect the right stats that apply to character.  dummy names are being used at present.  implement thenright constructor too
+}
